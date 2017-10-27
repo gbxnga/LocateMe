ffe1b98f5836a9eb5ac09c8cb08ad605a05112a6
kevin-deyoungster <kevin.deyoungster@gmail.com>
2017-10-27 20:44:14 +0000

    Converted the reversegeocoding function to use promises

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 require('dotenv').config();
 
 // Create a google maps client with apikey from environment
-var googleMapsClient = require('@google/maps').createClient({key: process.env.API_KEY});
+var googleMapsClient = require('@google/maps').createClient({
+    key: process.env.API_KEY
+});
 
-var express= require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
@@ -14,33 +16,49 @@ var cors = require('cors');
 app.use(cors());
 
 // Enable reading POST data from forms
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Setting up the frontend stuff
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Main Route
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.sendFile("index.html");
 });
 
 // Returns the Digital Address of GPS
-app.post('/address',function(req,res){
+app.post('/address', function (req, res) {
     var lat = parseFloat(req.body.lat);
     var lon = parseFloat(req.body.lon);
 
-    googleMapsClient.reverseGeocode({
-        latlng: [lat, lon]
-    }, function (err, response) {
-        if (!err) {
-            res.json(response.json.results[0]);
-        } else {
-            res.json("error");
-        }
+    let info = new Promise((resolve, reject) => {
+        googleMapsClient.reverseGeocode({
+            latlng: [lat, lon]
+        }, function (err, response) {
+            if (!err) {
+                geoinfo = response.json.results[0];
+                resolve(geoinfo);
+            } else {
+                reject(err);
+            }
+        })
     });
+
+    info.then(
+        function (val) {
+            console.log(val);
+        }
+    )
+    .catch(
+         function(reason) {
+            console.log(reason)
+        }
+    )
 })
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Listening on Port 3000");
 })
