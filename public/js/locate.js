6fa7e6b9e67ad74faac9bfa4f1d276f863f8e06b
Kevin de Youngster <kevin.deyoungster@gmail.com>
2019-06-22 15:17:04 +0000

    bug fix

--- public/js/locate.js
+++ public/js/locate.js
@@ -1,21 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
     txtAddress = $("#address");
     txtStatus = $("#status");
-})
+});
 
 function getLocation() {
-
     // Check if geolocation is supported
     if (navigator.geolocation) {
-
         var options = {
             enableHighAccuracy: true,
             timeout: 5000,
             maximumAge: 0
-        }
+        };
 
         navigator.geolocation.getCurrentPosition(showPositionAndGetDAddress, error, options);
-
     } else {
         txtStatus.text("Geolocation is not supported");
     }
@@ -25,34 +22,33 @@ function showPositionAndGetDAddress(position) {
     position = position.coords;
     txtStatus.text("[" + position.latitude + " , " + position.longitude + "]  " + position.accuracy + "m");
     getDAddress(position.latitude, position.longitude);
-
 }
 
 function error(err) {
     console.warn("Error occured in getting location");
+    console.warn(err);
 }
 
 function getDAddress(latitude, longitude) {
-
     latlon = {
         lat: latitude,
         lon: longitude
-    }
+    };
 
     url = "http://localhost:3000/address";
 
     $.ajax({
-        type: 'POST',
+        type: "POST",
         url: url,
         data: JSON.stringify(latlon),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function (response) {
+        success: function(response) {
             console.log(response);
             txtAddress.text(response.data);
         },
-        failure: function (error) {
+        failure: function(error) {
             txtAddress.text("Error Occurred");
         }
-    })
+    });
 }
--- server.js
+++ server.js
@@ -1,129 +1,123 @@
 // Necessary Tools
-var geo = require('./geo');
-var toolbox = require('./toolbox');
+var geo = require("./geo");
+var toolbox = require("./toolbox");
 
 // Load api key from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Create a google maps client with apikey from environment
-var googleMapsClient = require('@google/maps').createClient({
+var googleMapsClient = require("@google/maps").createClient({
     key: process.env.API_KEY
 });
 
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 // Required to allow client-side to make request to this server
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
 
 // Enable reading JSON data from client-side
 app.use(bodyParser.json());
 
 // Setting up the path to frontend
-app.use(express.static(require('path').join(__dirname, 'public')));
+app.use(express.static(require("path").join(__dirname, "public")));
 
 // Start the server
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("Listening on Port 3000");
-})
-
+});
 
 // Main Route
-app.get('/', function (req, res) {
+app.get("/", function(req, res) {
     res.sendFile("index.html");
 });
 
 // Returns the Digital Address of GPS
-app.post('/address', function (req, res) {
-
+app.post("/address", function(req, res) {
     var lat = parseFloat(req.body.lat);
     var lon = parseFloat(req.body.lon);
-    console.log('Checking for [' + lat + ',' + lon + ']');
+    console.log("Checking for [" + lat + "," + lon + "]");
 
     let ReverseGeoCode = new Promise((resolve, reject) => {
-        googleMapsClient.reverseGeocode({
-            latlng: [lat, lon]
-        }, function (err, response) {
-            if (!err) {
-                geoinfo = response.json.results[0];
-                resolve(geoinfo);
-            } else {
-                reject(err);
+        googleMapsClient.reverseGeocode(
+            {
+                latlng: [lat, lon]
+            },
+            function(err, response) {
+                if (!err) {
+                    geoinfo = response.json.results[0];
+                    resolve(geoinfo);
+                } else {
+                    reject(err);
+                }
             }
-        })
+        );
     });
 
-    ReverseGeoCode.then(
-        function (addressInfo) {
-            console.log(addressInfo.address_components[1].long_name);
-            addressComponents = addressInfo.address_components;
+    ReverseGeoCode.then(function(addressInfo) {
+        console.log(addressInfo.address_components[1].long_name);
+        addressComponents = addressInfo.address_components;
 
-            GetDigitalAddress(lat, lon, addressComponents)
-                .then(
-                (response) => { res.json({ result: "success", data: response }) }
-                )
-                .catch(
-                (error) => { console.log(error); res.json({ result: 'error', data: error }) }
-                )
-        })
-})
+        GetDigitalAddress(lat, lon, addressComponents)
+            .then(response => {
+                res.json({ result: "success", data: response });
+            })
+            .catch(error => {
+                console.log(error);
+                res.json({ result: "error", data: error });
+            });
+    });
+});
 
 // Returns promise of DigitalAddress
 function GetDigitalAddress(lat, lon, address_comps) {
-
     let info = new Promise((resolve, reject) => {
-        
         // Get Region
         region = GetRegion(address_comps);
-        
+
         // Get District
         district = GetDistrict(address_comps);
 
         // Get Center of District
         geo.Geocode(district)
-            .then(
-                (geoinfo) => {
+            .then(geoinfo => {
+                // Find distance from the point to center of district
+                distToCenter = geo.GetDistanceBetween(lat, lon, geoinfo.lat, geoinfo.lng);
 
-                    // Find distance from the point to center of district
-                    distToCenter = geo.GetDistanceBetween(lat, lon, geoinfo.lat, geoinfo.lng);
-                    
-                    // Convert distance to postcode
-                    postCode = (toolbox.zeroFill(Math.ceil(distToCenter / 500), 4));
+                // Convert distance to postcode
+                postCode = toolbox.zeroFill(Math.ceil(distToCenter / 500), 4);
 
-                    // Create unique address - Generates random number between 1 and 9999
-                    unique = toolbox.zeroFill(CreateUniqueAddress(),4)
-                    
-                    digitalAddress = region.substring(0, 1) + district.substring(0, 1) + '-' + postCode + '-' + unique 
-                    resolve(digitalAddress);
-                }
-            )
-            .catch(
-                (error) => {
-                    reject(error);
-                }
-            )
-    })
-    return info
+                // Create unique address - Generates random number between 1 and 9999
+                unique = toolbox.zeroFill(CreateUniqueAddress(), 4);
+
+                digitalAddress = region.substring(0, 1) + district.substring(0, 1) + "-" + postCode + "-" + unique;
+                resolve(digitalAddress);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+    return info;
 }
 
 // Returns Region from geoinfo
 function GetRegion(geoinfo) {
-    return (toolbox.Find('administrative_area_level_1', geoinfo));
+    return toolbox.Find("administrative_area_level_1", geoinfo);
 }
 
 // Returns District from geoinfo
 function GetDistrict(geoinfo) {
-    district = toolbox.Find('administrative_area_level_2', geoinfo);
+    district = toolbox.Find("administrative_area_level_2", geoinfo);
     if (district) {
         return district;
     } else {
-        return toolbox.Find('locality', geoinfo);
+        return toolbox.Find("locality", geoinfo);
     }
 }
 
-function CreateUniqueAddress(){
-    ad = Math.floor(Math.random() * 9999) + 1
+function CreateUniqueAddress() {
+    ad = Math.floor(Math.random() * 9999) + 1;
     return ad;
 }
